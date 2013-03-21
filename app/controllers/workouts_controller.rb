d435a5e500ab03be43eb2f139d49936d89e78325
MunkiPhD <munkiphd@gmail.com>
2013-03-20 23:55:55 -0400

    added more workout tests

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,20 +1,22 @@
 class WorkoutsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:show]
   before_action :set_workout, only: [:show, :edit, :update, :destroy]
 
   def index
+    @workouts = current_user.workouts
   end
 
   def show
   end
 
   def new
+    @workout = Workout.new
   end
 
   def create
-  def edit
-
   end
+
+  def edit
   end
 
   def update
@@ -32,4 +34,5 @@ class WorkoutsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def workout_params
       params.require(:workout).permit(:name, :title, :date_performed, :notes)
+    end
 end
--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,54 +1,83 @@
 require 'spec_helper'
 
 describe WorkoutsController do
+  before :each do
+      @user = create(:user)
+      @workout = create(:workout, user_id: @user.id)
+  end
 
-  describe "GET 'index'" do
-    it "returns http success" do
+  context "for authenticated user" do
+    before :each do
+      sign_in @user
+    end
+
+    describe "GET 'index'" do
+      it "returns a users workouts" do
+        get 'index'
+        response.should be_success
+        expect(assigns(:workouts)).to eq([@workout])
+      end
+    end
+
+
+    describe "GET 'show'" do
+      it "assigns the requested workout to @workout" do
+        get 'show', { id: @workout.id }
+        response.should be_success
+        expect(assigns(:workout)).to eq(@workout)
+      end
+
+      it "renders the :show template" do
+        get :show, id: @workout.id
+        expect(response).to render_template :show
+      end
+
+    end
+
+    describe "GET 'new'" do
+      it "assigns a new workout to @workout" do
+        get 'new'
+        expect(assigns(:workout)).to be_a_new(Workout)
+      end
+    end
+
+  
+    describe "GET 'edit'" do
+      it "returns http success" do
+        get 'edit', { id: @workout.id }
+        response.should be_success
+      end
+    end
+
+    pending "nested workout_sets"
+  end
+
+  context "for un-athenticated user" do
+
+    it "GET 'index' redirects to sign in" do
       get 'index'
-      response.should be_success
+      response.should redirect_to(new_user_session_path)
     end
-  end
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
+    it "GET 'show' is a success" do
+      get 'show', { id: @workout.id }
       response.should be_success
+      expect(assigns(:workout)).to eq(@workout)
     end
-  end
 
-  describe "GET 'new'" do
-    it "returns http success" do
+    it "GET 'new' redirects to sign in" do
       get 'new'
-      response.should be_success
+      response.should redirect_to(new_user_session_path)
     end
-  end
 
-  describe "GET 'create'" do
-    it "returns http success" do
+    it "GET 'edit' redirects to sign in" do
+      get :edit, id: @workout.id
+      response.should redirect_to(new_user_session_path)
+    end
+
+    it "POST 'create' redirects to sign in" do
       get 'create'
-      response.should be_success
+      response.should redirect_to(new_user_session_path)
     end
   end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
 end
