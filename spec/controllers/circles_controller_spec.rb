16e4021f09730b27a5300fa97a21fb6d0a71b0e6
MunkiPhD <munkiphd@gmail.com>
2013-04-12 09:05:28 -0400

    sped up tests

--- spec/controllers/circles_controller_spec.rb
+++ spec/controllers/circles_controller_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
 
 describe CirclesController do
-
+  let(:user) { create(:user) }
+  let(:circle) { create(:circle, user: user) }
+=begin
   before :each do
     @user = create(:user)
     @circle = create(:circle, user: @user)
   end
+=end
   context "for authenticated users" do
     before :each do
-      sign_in @user
+      sign_in user
     end
 
     describe "GET index" do
-      it "assigns all circles as @circles" do
+      it "assigns all circles as circles" do
         get :index, {}
-        expect(assigns(:circles)).to match_array [@circle]
+        expect(assigns(:circles)).to match_array [circle]
       end
 
       describe "JSON" do
@@ -24,9 +27,9 @@ describe CirclesController do
 
 
     describe "GET show" do
-      it "assigns the requested circle as @circle" do
-        get :show, {:id => @circle.to_param}
-        expect(assigns(:circle)).to eq(@circle)
+      it "assigns the requested circle as circle" do
+        get :show, {:id => circle.to_param}
+        expect(assigns(:circle)).to eq(circle)
       end
 
       describe "JSON" do
@@ -35,16 +38,16 @@ describe CirclesController do
     end
 
     describe "GET new" do
-      it "assigns a new circle as @circle" do
+      it "assigns a new circle as circle" do
         get :new, {}
         expect(assigns(:circle)).to be_a_new(Circle)
       end
     end
 
     describe "GET edit" do
-      it "assigns the requested circle as @circle" do
-        get :edit, {:id => @circle.to_param}
-        expect(assigns(:circle)).to eq(@circle)
+      it "assigns the requested circle as circle" do
+        get :edit, {:id => circle.to_param}
+        expect(assigns(:circle)).to eq(circle)
       end
     end
 
@@ -56,7 +59,7 @@ describe CirclesController do
           }.to change(Circle, :count).by(1)
         end
 
-        it "assigns a newly created circle as @circle" do
+        it "assigns a newly created circle as circle" do
           post :create, {:circle => attributes_for(:circle)}
           expect(assigns(:circle)).to be_a(Circle)
           expect(assigns(:circle)).to be_persisted
@@ -73,7 +76,7 @@ describe CirclesController do
       end
 
       describe "with invalid params" do
-        it "assigns a newly created but unsaved circle as @circle" do
+        it "assigns a newly created but unsaved circle as circle" do
           # Trigger the behavior that occurs when invalid params are submitted
           Circle.any_instance.stub(:save).and_return(false)
           post :create, {:circle => { "name" => "invalid value" }}
@@ -101,17 +104,17 @@ describe CirclesController do
           # receives the :update_attributes message with whatever params are
           # submitted in the request.
           Circle.any_instance.should_receive(:update).with({ "name" => "MyString" })
-          put :update, {:id => @circle.to_param, :circle => { "name" => "MyString" }}
+          put :update, {:id => circle.to_param, :circle => { "name" => "MyString" }}
         end
 
-        it "assigns the requested circle as @circle" do
-          put :update, {:id => @circle.to_param, :circle => attributes_for(:circle)}
-          expect(assigns(:circle)).to eq(@circle)
+        it "assigns the requested circle as circle" do
+          put :update, {:id => circle.to_param, :circle => attributes_for(:circle)}
+          expect(assigns(:circle)).to eq(circle)
         end
 
         it "redirects to the circle" do
-          put :update, {:id => @circle.to_param, :circle => attributes_for(:circle)}
-          expect(response).to redirect_to(@circle)
+          put :update, {:id => circle.to_param, :circle => attributes_for(:circle)}
+          expect(response).to redirect_to(circle)
         end
 
         describe "JSON" do
@@ -120,17 +123,17 @@ describe CirclesController do
       end
 
       describe "with invalid params" do
-        it "assigns the circle as @circle" do
+        it "assigns the circle as circle" do
           # Trigger the behavior that occurs when invalid params are submitted
           Circle.any_instance.stub(:save).and_return(false)
-          put :update, {:id => @circle.to_param, :circle => { "name" => "invalid value" }}
-          expect(assigns(:circle)).to eq(@circle)
+          put :update, {:id => circle.to_param, :circle => { "name" => "invalid value" }}
+          expect(assigns(:circle)).to eq(circle)
         end
 
         it "re-renders the 'edit' template" do
           # Trigger the behavior that occurs when invalid params are submitted
           Circle.any_instance.stub(:save).and_return(false)
-          put :update, {:id => @circle.to_param, :circle => { "name" => "invalid value" }}
+          put :update, {:id => circle.to_param, :circle => { "name" => "invalid value" }}
           expect(response).to render_template("edit")
         end
 
@@ -141,14 +144,15 @@ describe CirclesController do
     end
 
     describe "DELETE destroy" do
-      it "destroys the requested circle" do
+      it "the requested circle" do
+        circle = create(:circle)
         expect {
-          delete :destroy, {:id => @circle.to_param}
+          delete :destroy, id: circle.to_param
         }.to change(Circle, :count).by(-1)
       end
 
       it "redirects to the circles list" do
-        delete :destroy, {:id => @circle.to_param}
+        delete :destroy, {:id => circle.to_param}
         expect(response).to redirect_to(circles_url)
       end
 
@@ -163,13 +167,13 @@ describe CirclesController do
   context "for un-athenticated user" do
     it "GET 'index' redirects to sign in" do
       get :index
-      expect(assigns(:circles)).to match_array [@circle]
+      expect(assigns(:circles)).to match_array [circle]
     end
 
-    it "GET 'show' assigns the specified circle to @circle" do
-      get :show,  id: @circle.id 
+    it "GET 'show' assigns the specified circle to circle" do
+      get :show,  id: circle.id 
       response.should be_success
-      expect(assigns(:circle)).to eq(@circle)
+      expect(assigns(:circle)).to eq(circle)
     end
 
     it "GET 'new' redirects to sign in" do
@@ -178,7 +182,7 @@ describe CirclesController do
     end
 
     it "GET 'edit' redirects to sign in" do
-      get :edit, id: @circle.id
+      get :edit, id: circle.id
       expect(response).to redirect_to(new_user_session_path)
     end
 
@@ -205,7 +209,7 @@ describe CirclesController do
       end
 
       it "redirects to the user login" do
-        put :update, id: @circle, circle: attributes_for(:circle)
+        put :update, id: circle, circle: attributes_for(:circle)
         expect(response).to redirect_to new_user_session_path
       end
     end
