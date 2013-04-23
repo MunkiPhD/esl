3b3c22b2a7f7c5724a5a12b08b45dbf38c3608d9
MunkiPhD <munkiphd@gmail.com>
2013-04-23 14:32:06 -0400

    fixed minor bug when user is not signed in

--- app/helpers/circles_helper.rb
+++ app/helpers/circles_helper.rb
@@ -1,5 +1,8 @@
 module CirclesHelper
   def join_leave_link(circle)
+    if current_user.blank?
+      return
+    end
     if current_user.has_any_role? :god, { name: :member, resource: circle }, { name: :admin, resource: circle }
       # show the leave link unless theyre the creator of the circle
       button_to 'Leave', leave_circle_path(circle.id), method: :post unless current_user == circle.user
