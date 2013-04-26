c0ea7fef771dfb4e4f8afa829061caddf6153ea7
MunkiPhD <munkiphd@gmail.com>
2013-04-26 15:14:17 -0400

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def format_date(date)
+    unless date.nil?
+      date.strftime('%d-%B-%Y')
+    end
+  end
+
+  def days_ago(date)
+    if date == Date.today
+      return "Today"
+    else
+      parsed_time = Time.parse(date.strftime('%d-%b-%y')) + (-Time.zone_offset(Time.now.zone))
+      return "#{time_ago_in_words(parsed_time)} ago"
+    end
+  end
 end
