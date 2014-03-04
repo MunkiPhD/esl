23291aabe099fc4e827a261d6d9e91bedb1a5d31
MunkiPhD <munkiphd@gmail.com>
2014-03-04 09:10:57 -0500

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130830011149) do
+ActiveRecord::Schema.define(version: 20130707053501) do
 
   create_table "circles", force: true do |t|
     t.string   "name",                       null: false
@@ -32,73 +32,66 @@ ActiveRecord::Schema.define(version: 20130830011149) do
     t.datetime "updated_at"
   end
 
-  create_table "food_servings", force: true do |t|
-    t.integer  "food_id",                                   null: false
-    t.string   "serving_size",        default: "1 serving", null: false
-    t.integer  "calories",            default: 0,           null: false
-    t.integer  "calories_from_fat",   default: 0,           null: false
-    t.decimal  "total_fat",           default: 0.0,         null: false
-    t.decimal  "saturated_fat",       default: 0.0,         null: false
-    t.decimal  "trans_fat",           default: 0.0,         null: false
-    t.decimal  "polyunsaturated_fat", default: 0.0,         null: false
-    t.decimal  "monounsaturated_fat", default: 0.0,         null: false
-    t.decimal  "cholesterol",         default: 0.0,         null: false
-    t.decimal  "sodium",              default: 0.0,         null: false
-    t.decimal  "carbs",               default: 0.0,         null: false
-    t.decimal  "dietary_fiber",       default: 0.0,         null: false
-    t.decimal  "sugars",              default: 0.0,         null: false
-    t.decimal  "protein",             default: 0.0,         null: false
-    t.integer  "vitamin_a",           default: 0,           null: false
-    t.integer  "vitamin_c",           default: 0,           null: false
-    t.integer  "calcium",             default: 0,           null: false
-    t.integer  "iron",                default: 0,           null: false
-    t.integer  "vitamin_d",           default: 0,           null: false
-    t.integer  "vitamin_e",           default: 0,           null: false
-    t.integer  "vitamin_k",           default: 0,           null: false
-    t.integer  "thiamin",             default: 0,           null: false
-    t.integer  "riboflavin",          default: 0,           null: false
-    t.integer  "niacin",              default: 0,           null: false
-    t.integer  "vitamin_b6",          default: 0,           null: false
-    t.integer  "biotin",              default: 0,           null: false
-    t.integer  "pantothenic_acid",    default: 0,           null: false
-    t.integer  "phosphorus",          default: 0,           null: false
-    t.integer  "iodine",              default: 0,           null: false
-    t.integer  "magnesium",           default: 0,           null: false
-    t.integer  "zinc",                default: 0,           null: false
-    t.integer  "selenium",            default: 0,           null: false
-    t.integer  "copper",              default: 0,           null: false
-    t.integer  "manganese",           default: 0,           null: false
-    t.integer  "chromium",            default: 0,           null: false
-    t.integer  "molybednum",          default: 0,           null: false
-    t.integer  "caffeine",            default: 0,           null: false
-    t.integer  "alcohol",             default: 0,           null: false
-    t.decimal  "potassium",           default: 0.0,         null: false
-    t.integer  "folic_acid",          default: 0,           null: false
-    t.decimal  "boron",               default: 0.0,         null: false
-    t.decimal  "decimal",             default: 0.0,         null: false
-    t.decimal  "cobalt",              default: 0.0,         null: false
-    t.decimal  "chloride",            default: 0.0,         null: false
-    t.decimal  "fluoride",            default: 0.0,         null: false
-    t.decimal  "acetic_acid",         default: 0.0,         null: false
-    t.decimal  "citric_acid",         default: 0.0,         null: false
-    t.decimal  "lactic_acid",         default: 0.0,         null: false
-    t.decimal  "malic_acid",          default: 0.0,         null: false
-    t.decimal  "choline",             default: 0.0,         null: false
-    t.decimal  "taurine",             default: 0.0,         null: false
-    t.decimal  "glutamine",           default: 0.0,         null: false
-    t.decimal  "creatine",            default: 0.0,         null: false
-    t.decimal  "sugar_alcohols",      default: 0.0,         null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "foods", force: true do |t|
-    t.string   "name",                  default: ""
-    t.string   "brand",                 default: ""
-    t.string   "ndb_no",      limit: 6, default: ""
-    t.text     "ingredients",           default: ""
-    t.boolean  "usda",                  default: false
-    t.decimal  "decimal",               default: 0.0,   null: false
+    t.string   "name",                          default: ""
+    t.string   "brand",                         default: ""
+    t.string   "ndb_no",              limit: 6, default: ""
+    t.text     "ingredients",                   default: ""
+    t.boolean  "usda",                          default: false
+    t.string   "serving_size",                  default: "1 serving", null: false
+    t.integer  "calories",                      default: 0,           null: false
+    t.integer  "calories_from_fat",             default: 0,           null: false
+    t.decimal  "total_fat",                     default: 0.0,         null: false
+    t.decimal  "saturated_fat",                 default: 0.0,         null: false
+    t.decimal  "trans_fat",                     default: 0.0,         null: false
+    t.decimal  "polyunsaturated_fat",           default: 0.0,         null: false
+    t.decimal  "monounsaturated_fat",           default: 0.0,         null: false
+    t.decimal  "cholesterol",                   default: 0.0,         null: false
+    t.decimal  "sodium",                        default: 0.0,         null: false
+    t.decimal  "carbs",                         default: 0.0,         null: false
+    t.decimal  "dietary_fiber",                 default: 0.0,         null: false
+    t.decimal  "sugars",                        default: 0.0,         null: false
+    t.decimal  "protein",                       default: 0.0,         null: false
+    t.integer  "vitamin_a",                     default: 0,           null: false
+    t.integer  "vitamin_c",                     default: 0,           null: false
+    t.integer  "calcium",                       default: 0,           null: false
+    t.integer  "iron",                          default: 0,           null: false
+    t.integer  "vitamin_d",                     default: 0,           null: false
+    t.integer  "vitamin_e",                     default: 0,           null: false
+    t.integer  "vitamin_k",                     default: 0,           null: false
+    t.integer  "thiamin",                       default: 0,           null: false
+    t.integer  "riboflavin",                    default: 0,           null: false
+    t.integer  "niacin",                        default: 0,           null: false
+    t.integer  "vitamin_b6",                    default: 0,           null: false
+    t.integer  "biotin",                        default: 0,           null: false
+    t.integer  "pantothenic_acid",              default: 0,           null: false
+    t.integer  "phosphorus",                    default: 0,           null: false
+    t.integer  "iodine",                        default: 0,           null: false
+    t.integer  "magnesium",                     default: 0,           null: false
+    t.integer  "zinc",                          default: 0,           null: false
+    t.integer  "selenium",                      default: 0,           null: false
+    t.integer  "copper",                        default: 0,           null: false
+    t.integer  "manganese",                     default: 0,           null: false
+    t.integer  "chromium",                      default: 0,           null: false
+    t.integer  "molybednum",                    default: 0,           null: false
+    t.integer  "caffeine",                      default: 0,           null: false
+    t.integer  "alcohol",                       default: 0,           null: false
+    t.decimal  "potassium",                     default: 0.0,         null: false
+    t.integer  "folic_acid",                    default: 0,           null: false
+    t.decimal  "boron",                         default: 0.0,         null: false
+    t.decimal  "decimal",                       default: 0.0,         null: false
+    t.decimal  "cobalt",                        default: 0.0,         null: false
+    t.decimal  "chloride",                      default: 0.0,         null: false
+    t.decimal  "fluoride",                      default: 0.0,         null: false
+    t.decimal  "acetic_acid",                   default: 0.0,         null: false
+    t.decimal  "citric_acid",                   default: 0.0,         null: false
+    t.decimal  "lactic_acid",                   default: 0.0,         null: false
+    t.decimal  "malic_acid",                    default: 0.0,         null: false
+    t.decimal  "choline",                       default: 0.0,         null: false
+    t.decimal  "taurine",                       default: 0.0,         null: false
+    t.decimal  "glutamine",                     default: 0.0,         null: false
+    t.decimal  "creatine",                      default: 0.0,         null: false
+    t.decimal  "sugar_alcohols",                default: 0.0,         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -115,19 +108,20 @@ ActiveRecord::Schema.define(version: 20130830011149) do
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                           default: "", null: false
+    t.string   "username",               default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
