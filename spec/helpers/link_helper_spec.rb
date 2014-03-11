55a25c0dc127d7ea0b4fc82c1e10a165fe4611b7
MunkiPhD <munkiphd@gmail.com>
2014-03-11 19:09:49 -0400

    added test specs

--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
 
 describe LinkHelper do
-  describe "#add_link" do
+  describe "#link_to_add" do
     it "links to the specified url" do
       exercise = create(:exercise)
-      expect(helper.add_link("test", exercise)).to include("test")
-      expect(helper.add_link("test", exercise)).to include(exercise_path(exercise))
-      expect(helper.add_link("test", exercise)).to include("icon-plus icon-white")
+      expect(helper.link_to_add("test", exercise)).to include("test")
+      expect(helper.link_to_add("test", exercise)).to include(exercise_path(exercise))
+      expect(helper.link_to_add("test", exercise)).to include("icon-plus icon-white")
+      expect(helper.link_to_add("test", exercise)).to include("btn btn-success")
+    end
+  end
+
+  describe "#link_to_destroy" do
+    it "creates a button to remove an item" do
+      exercise = create(:exercise)
+      text = "delete item"
+      expect(helper.link_to_destroy(text, exercise)).to include(text)
+      expect(helper.link_to_destroy(text, exercise)).to include(exercise_path(exercise)) 
+      expect(helper.link_to_destroy(text, exercise)).to include("data-method=\"delete\"") 
+      expect(helper.link_to_destroy(text, exercise)).to include("btn btn-danger") 
+      expect(helper.link_to_destroy(text, exercise)).to include("data-confirm=\"Are you sure?\"") 
+      expect(helper.link_to_destroy(text, exercise)).to include("icon-remove icon-white")
     end
   end
 end
