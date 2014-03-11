7a63f89236f97278a3d912e213f2b2c0183cbe1b
MunkiPhD <munkiphd@gmail.com>
2014-03-11 19:37:18 -0400

    added edit and additional tests

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,6 +1,6 @@
 module LinkHelper
   def link_to_add(body, url, html_options = {})
-    html_options[:class] = "btn btn-success"
+    set_html_options(html_options, "btn btn-success")
     link_to url, html_options do
       content_tag(:i, nil, class: "icon-plus icon-white") + " " + body
     end
@@ -8,12 +8,27 @@ module LinkHelper
 
 
   def link_to_destroy(body, url, html_options = {})
-    html_options[:class] ||= ""
-    html_options[:class] << "btn btn-danger"
+    set_html_options(html_options, "btn btn-danger")
     html_options[:method] = :delete
     html_options[:data] = { confirm: "Are you sure?" }
     link_to url, html_options do
       content_tag(:i, nil, class: "icon-remove icon-white") + " " + body
     end
+  end
+
+
+  def link_to_edit(body, url, html_options = {})
+    set_html_options(html_options, "btn btn-primary")
+    link_to url, html_options do
+      content_tag(:i, nil, class: "icon-pencil icon-white") + " " + body
+    end
+  end
+
+
+  private
+  def set_html_options(html_options = {}, additional_options)
+    html_options[:class] ||= ""
+    html_options[:class] << " " + additional_options
+    html_options
   end
 end
--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -4,6 +4,7 @@ describe LinkHelper do
   let(:text) { "url text" }
   let(:item) { create(:exercise) }
   let(:item_path) { exercise_path(item) }
+  let(:item_edit_path) { edit_exercise_path(item) }
 
   describe "#link_to_add" do
     it "links to the specified url" do
@@ -12,6 +13,11 @@ describe LinkHelper do
       expect(result).to include(item_path)
       expect(result).to include("icon-plus icon-white")
       expect(result).to include("btn btn-success")
+    end
+
+    it "does not conflict with other html options" do
+      result = link_to_add(text, item, class: "test")
+      expect(result).to include("test btn btn-success")
     end
   end
 
@@ -24,6 +30,26 @@ describe LinkHelper do
       expect(result).to include("btn btn-danger") 
       expect(result).to include("data-confirm=\"Are you sure?\"") 
       expect(result).to include("icon-remove icon-white")
+    end
+
+    it "does not conflict with other html options" do
+      result = link_to_destroy(text, item, class: "test")
+      expect(result).to include("test btn btn-danger")
+    end
+  end
+
+  describe "#link_to_edit" do
+    it "creates a link to the edit page" do
+      result = link_to_edit(text, item_edit_path)
+      expect(result).to include(text)
+      expect(result).to include(item_edit_path)
+      expect(result).to include("btn btn-primary")
+      expect(result).to include("icon-pencil icon-white")
+    end
+
+    it "does not conflict with other html options" do
+      result = link_to_edit(text, item_edit_path, class: "test")
+      expect(result).to include("test btn btn-primary")
     end
   end
 end
