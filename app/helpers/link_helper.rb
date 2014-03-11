420f3d6d658066c72defc8e76e317937b0646e34
MunkiPhD <munkiphd@gmail.com>
2014-03-11 19:10:02 -0400

    added helper

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,8 +1,19 @@
 module LinkHelper
-  def add_link(body, url, html_options = {})
+  def link_to_add(body, url, html_options = {})
     html_options[:class] = "btn btn-success"
     link_to url, html_options do
       content_tag(:i, nil, class: "icon-plus icon-white") + " " + body
+    end
+  end
+
+
+  def link_to_destroy(body, url, html_options = {})
+    html_options[:class] ||= ""
+    html_options[:class] << "btn btn-danger"
+    html_options[:method] = :delete
+    html_options[:data] = { confirm: "Are you sure?" }
+    link_to url, html_options do
+      content_tag(:i, nil, class: "icon-remove icon-white") + " " + body
     end
   end
 end
