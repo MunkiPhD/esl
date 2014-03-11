ff006296d7a8a3a9977dffa51b138adcfb20d62b
MunkiPhD <munkiphd@gmail.com>
2014-03-11 19:14:54 -0400

    test refactor

--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -1,26 +1,27 @@
 require 'spec_helper'
 
 describe LinkHelper do
+  let(:text) { "url text" }
+  let(:item) { create(:exercise) }
+  let(:item_path) { exercise_path(item) }
+
   describe "#link_to_add" do
     it "links to the specified url" do
-      exercise = create(:exercise)
-      expect(helper.link_to_add("test", exercise)).to include("test")
-      expect(helper.link_to_add("test", exercise)).to include(exercise_path(exercise))
-      expect(helper.link_to_add("test", exercise)).to include("icon-plus icon-white")
-      expect(helper.link_to_add("test", exercise)).to include("btn btn-success")
+      expect(helper.link_to_add(text, item)).to include(text)
+      expect(helper.link_to_add(text, item)).to include(item_path)
+      expect(helper.link_to_add(text, item)).to include("icon-plus icon-white")
+      expect(helper.link_to_add(text, item)).to include("btn btn-success")
     end
   end
 
   describe "#link_to_destroy" do
     it "creates a button to remove an item" do
-      exercise = create(:exercise)
-      text = "delete item"
-      expect(helper.link_to_destroy(text, exercise)).to include(text)
-      expect(helper.link_to_destroy(text, exercise)).to include(exercise_path(exercise)) 
-      expect(helper.link_to_destroy(text, exercise)).to include("data-method=\"delete\"") 
-      expect(helper.link_to_destroy(text, exercise)).to include("btn btn-danger") 
-      expect(helper.link_to_destroy(text, exercise)).to include("data-confirm=\"Are you sure?\"") 
-      expect(helper.link_to_destroy(text, exercise)).to include("icon-remove icon-white")
+      expect(helper.link_to_destroy(text, item)).to include(text)
+      expect(helper.link_to_destroy(text, item)).to include(item_path) 
+      expect(helper.link_to_destroy(text, item)).to include("data-method=\"delete\"") 
+      expect(helper.link_to_destroy(text, item)).to include("btn btn-danger") 
+      expect(helper.link_to_destroy(text, item)).to include("data-confirm=\"Are you sure?\"") 
+      expect(helper.link_to_destroy(text, item)).to include("icon-remove icon-white")
     end
   end
 end
