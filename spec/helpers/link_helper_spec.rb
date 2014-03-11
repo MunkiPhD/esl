a46d46bd1285c70abacbbe1dd1abc905321fa54f
MunkiPhD <munkiphd@gmail.com>
2014-03-11 19:16:16 -0400

    more test refactor

--- spec/helpers/link_helper_spec.rb
+++ spec/helpers/link_helper_spec.rb
@@ -7,21 +7,23 @@ describe LinkHelper do
 
   describe "#link_to_add" do
     it "links to the specified url" do
-      expect(helper.link_to_add(text, item)).to include(text)
-      expect(helper.link_to_add(text, item)).to include(item_path)
-      expect(helper.link_to_add(text, item)).to include("icon-plus icon-white")
-      expect(helper.link_to_add(text, item)).to include("btn btn-success")
+      result = helper.link_to_add(text, item)
+      expect(result).to include(text)
+      expect(result).to include(item_path)
+      expect(result).to include("icon-plus icon-white")
+      expect(result).to include("btn btn-success")
     end
   end
 
   describe "#link_to_destroy" do
     it "creates a button to remove an item" do
-      expect(helper.link_to_destroy(text, item)).to include(text)
-      expect(helper.link_to_destroy(text, item)).to include(item_path) 
-      expect(helper.link_to_destroy(text, item)).to include("data-method=\"delete\"") 
-      expect(helper.link_to_destroy(text, item)).to include("btn btn-danger") 
-      expect(helper.link_to_destroy(text, item)).to include("data-confirm=\"Are you sure?\"") 
-      expect(helper.link_to_destroy(text, item)).to include("icon-remove icon-white")
+      result = helper.link_to_destroy(text, item)
+      expect(result).to include(text)
+      expect(result).to include(item_path) 
+      expect(result).to include("data-method=\"delete\"") 
+      expect(result).to include("btn btn-danger") 
+      expect(result).to include("data-confirm=\"Are you sure?\"") 
+      expect(result).to include("icon-remove icon-white")
     end
   end
 end
