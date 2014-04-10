f7bd0f3a09417b7bd14b1c7384489c22bd5b4ea1
MunkiPhD <munkiphd@gmail.com>
2014-04-10 19:39:17 -0400

    more specs

--- spec/features/nutrition/logged_foods/log_food_spec.rb
+++ spec/features/nutrition/logged_foods/log_food_spec.rb
@@ -2,17 +2,33 @@ require 'spec_helper'
 
 feature "user logs a food item" do
   let(:food) { create(:bread) }
+  let(:user) { create(:user) }
 
-  scenario "user searches for a food and logs it" do
-    visit foods_path
-    fill_in 'search', with: food.name
-    click_button 'Search'
-    click_link food.name
-    expect(page).to have_content food.name
-    click_link 'Log Item'
-    fill_in 'Servings', with: "1"
-    fill_in 'Date', with: Date.today.to_s
-    click_button 'Log'
-    expect(page).to have_content "Logged 1 serving of #{food.name}"
+  context 'visitors' do
+    scenario 'attempts to log food, is redirected' do
+      visit food_path(food)
+      click_link 'Log Item'
+      expect(page).to have_content("You need to sign in or sign up before continuing.")
+    end
+  end
+
+  context 'logged in users' do
+    before :each do 
+      login_user user
+    end
+
+    scenario "user searches for a food and logs it" do
+      visit foods_path
+      fill_in 'search', with: food.name
+      click_button 'Search'
+      click_link food.name
+      expect(page).to have_content food.name
+      click_link 'Log Item'
+      fill_in 'Servings', with: "1"
+      fill_in 'Date', with: Date.today.to_s
+      click_button 'Log'
+      expect(page).to have_content "Logged 1 serving of #{food.name}"
+    end
+
   end
 end
