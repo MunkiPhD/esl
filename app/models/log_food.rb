ba0995ebe0f92b1b78b5d05c2b80c312e0a3f91e
MunkiPhD <munkiphd@gmail.com>
2014-04-10 18:52:55 -0400

    db annotations

--- app/models/log_food.rb
+++ app/models/log_food.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: log_foods
+#
+#  id         :integer          not null, primary key
+#  servings   :decimal(, )      default(1.0), not null
+#  log_date   :date             default(Thu, 10 Apr 2014), not null
+#  food_id    :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class LogFood < ActiveRecord::Base
   belongs_to :user
   belongs_to :food
--- spec/factories/log_foods.rb
+++ spec/factories/log_foods.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: log_foods
+#
+#  id         :integer          not null, primary key
+#  servings   :decimal(, )      default(1.0), not null
+#  log_date   :date             default(Thu, 10 Apr 2014), not null
+#  food_id    :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
