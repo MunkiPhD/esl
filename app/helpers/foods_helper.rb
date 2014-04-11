b39482678d3a2be2f453437d2c4eb71bc9bebca7
MunkiPhD <munkiphd@gmail.com>
2014-04-11 07:55:53 -0400

    modified foods

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,13 @@
 module FoodsHelper
+  def display_vitamin(display_name, value)
+    unless value == 0
+      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{value.to_s}%</td></tr>")
+    end
+  end
+
+  def display_custom(display_name, value, unit)
+    unless value == 0
+      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{value.to_s}#{unit}</td></tr>")
+    end
+  end
 end
