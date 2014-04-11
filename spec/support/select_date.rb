fd05e1dd3dc768317cffe3e2d281855cf2be99b2
MunkiPhD <munkiphd@gmail.com>
2014-04-10 20:04:42 -0400

    comments

--- spec/support/select_date.rb
+++ spec/support/select_date.rb
@@ -2,7 +2,7 @@
 # http://stackoverflow.com/questions/6729786/how-to-select-date-from-a-select-box-using-capybara-in-rails-3
 #
 module SelectDateHelper
-  # use as so: '2014, jan, 1'
+  # use as so: '2014,January, 1'
   def select_date(date, options = {})
     field = options[:from]
     base_id = find(:xpath, ".//label[contains(.,'#{field}')]")[:for]
