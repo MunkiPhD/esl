07578843a18f5dc36f8bb1b77310bcecdf800b28
MunkiPhD <munkiphd@gmail.com>
2014-04-12 09:50:16 -0400

    added date selecting

--- app/controllers/nutrition_controller.rb
+++ app/controllers/nutrition_controller.rb
@@ -3,5 +3,21 @@ class NutritionController < ApplicationController
 
   def index
     @logged_foods = current_user.log_foods
+    @current_date = selected_date 
+  end
+
+
+  private
+  def selected_date
+    d = Date.new(date_params[:year].to_i, date_params[:month].to_i, date_params[:day].to_i) rescue nil
+    if d
+      return d
+    else
+      return Date.today
+    end
+  end
+
+  def date_params
+    params.require(:log_date)
   end
 end
--- spec/features/nutrition/dashboard_spec.rb
+++ spec/features/nutrition/dashboard_spec.rb
@@ -14,5 +14,33 @@ feature "user visits the dashboard" do
       expect(page).to have_content "Nutrition Dashboard"
       expect(page).to have_content "No foods logged yet for today!"
     end
+
+    scenario 'elects to view a different date' do
+      visit root_path
+      click_link "nutrition"
+      within '#selected_date' do
+        expect(page).to have_content "Today"
+      end
+      date = Date.yesterday
+      puts "yesterdays date:=========================== #{date}"
+
+      select date.day.to_s, from: 'log_date_day'
+      select date.strftime("%B"), from: 'log_date_month'
+      select date.year.to_s, from: 'log_date_year'
+
+      click_button 'Go'
+      expect(page).to have_content "1 day ago"
+      expect(page).to have_link "Go back to today"
+
+
+      # go back to a date in a previous year
+      date = 2.years.ago
+      select date.day.to_s, from: 'log_date_day'
+      select date.strftime("%B"), from: 'log_date_month'
+      select date.year.to_s, from: 'log_date_year'
+      click_button 'Go'
+      expect(page).to have_content "about 2 years ago"
+      expect(page).to have_link "Go back to today"
+    end
   end
 end
