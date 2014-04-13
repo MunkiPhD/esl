38b36d1158c426cd9d888da9211840b2aa4c1e35
MunkiPhD <munkiphd@gmail.com>
2014-04-13 16:46:32 -0400

    css styling and helper modification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,11 @@ module ApplicationHelper
       return "Today"
     else
       parsed_time = Time.parse(date.strftime('%d-%b-%y')) + (-Time.zone_offset(Time.now.zone))
-      return "#{time_ago_in_words(parsed_time)} ago"
+      if parsed_time > Date.today
+        return "#{time_ago_in_words(parsed_time)} in the future"
+      else
+        return "#{time_ago_in_words(parsed_time)} ago"
+      end
     end
   end
 end
