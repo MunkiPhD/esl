0d13acec0125f6ec3abf80fb571863e6fb76501d
MunkiPhD <munkiphd@gmail.com>
2014-04-21 09:00:55 -0400

    add food name to json response

--- app/views/log_foods/create.json.jbuilder
+++ app/views/log_foods/create.json.jbuilder
@@ -1,2 +1,3 @@
 json.location log_food_path(@logged_food)
 json.log_food @logged_food
+json.food_name @logged_food.food_name
--- spec/controllers/log_foods_controller_spec.rb
+++ spec/controllers/log_foods_controller_spec.rb
@@ -51,15 +51,18 @@ describe LogFoodsController do
     end
   end
 
-	describe "POST 'create'" do
+	describe "POST 'create' json" do
 		render_views
 
-		it 'creates the item with valid json' do
+		it 'creates the item and returns correct details' do
 			food = create(:food)
 			log_food = build(:log_food, user: user, food: food)
 			post :create, { :format => 'json', :log_food => log_food.attributes, :food_id => food.id }
 			puts response.body
+			parsed_json = JSON.parse(response.body)
 			expect(response.status).to eq 201
+			expect(parsed_json["food_name"]).to eq food.name
+			expect(parsed_json["log_food"]["servings"]).to eq sprintf("%.02f", log_food.servings)
 		end
 
 		it 'returns errors if missing attributes' do
