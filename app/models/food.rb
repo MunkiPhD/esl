29bb0724f46390a4e0792038f1fc400f9706565e
MunkiPhD <munkiphd@gmail.com>
2014-04-22 14:59:08 -0400

    add validation and added image to favorites

--- app/models/food.rb
+++ app/models/food.rb
@@ -67,102 +67,99 @@
 #
 
 class Food < ActiveRecord::Base
-  has_many :log_foods
+	has_many :log_foods
 	has_attached_file :food_image,
 		styles: { medium: "250x250", thumb: "50x50" }, 
-		default_url: ":style/apple.png",
-		size: { in: 0..2.megabytes }
-	#, 
-	#	styles: { medium: "250x250", thumb: "50x50" }, 
-	#	default_url: ":style/apple.png",
-	#	size: { in: 0..2.megabytes }
+		default_url: ":style/apple.png"
+
 	validates_attachment :food_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+	validates_attachment_size :food_image, less_than_or_equal_to: 2.megabytes, if: Proc.new { |import| !import.food_image.file? }
 
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 150 }, allow_blank: false
-  validates :serving_size, presence: true, length: { minimum: 1, maximum: 75 }
-  validates_numericality_of :calories, 
-                            :calories_from_fat,
-                            :total_fat,
-                            :saturated_fat,
-                            :trans_fat,
-                            :polyunsaturated_fat,
-                            :monounsaturated_fat,
-                            :cholesterol,
-                            :sodium,
-                            :carbs,
-                            :dietary_fiber,
-                            :sugars,
-                            :protein,
-                            :vitamin_a,
-                            :vitamin_c,
-                            :calcium,
-                            :iron,
-                            :vitamin_d,
-                            :vitamin_e,
-                            :vitamin_k,
-                            :thiamin,
-                            :riboflavin,
-                            :niacin,
-                            :vitamin_b6,
-                            :biotin,
-                            :pantothenic_acid,
-                            :phosphorus,
-                            :iodine,
-                            :magnesium,
-                            :zinc,
-                            :selenium,
-                            :copper,
-                            :manganese,
-                            :chromium,
-                            :molybednum,
-                            :caffeine,
-                            :alcohol,
-                            :potassium,
-                            :folic_acid,
-                            :boron,
-                            :cobalt,
-                            :fluoride,
-                            :acetic_acid,
-                            :citric_acid,
-                            :lactic_acid,
-                            :malic_acid,
-                            :choline,
-                            :taurine,
-                            :glutamine,
-                            :creatine,
-                            :sugar_alcohols,
-                            :chloride,
-                            greater_than_or_equal_to: 0
+	validates :name, presence: true, length: { minimum: 2, maximum: 150 }, allow_blank: false
+	validates :serving_size, presence: true, length: { minimum: 1, maximum: 75 }
+	validates_numericality_of :calories, 
+		:calories_from_fat,
+		:total_fat,
+		:saturated_fat,
+		:trans_fat,
+		:polyunsaturated_fat,
+		:monounsaturated_fat,
+		:cholesterol,
+		:sodium,
+		:carbs,
+		:dietary_fiber,
+		:sugars,
+		:protein,
+		:vitamin_a,
+		:vitamin_c,
+		:calcium,
+		:iron,
+		:vitamin_d,
+		:vitamin_e,
+		:vitamin_k,
+		:thiamin,
+		:riboflavin,
+		:niacin,
+		:vitamin_b6,
+		:biotin,
+		:pantothenic_acid,
+		:phosphorus,
+		:iodine,
+		:magnesium,
+		:zinc,
+		:selenium,
+		:copper,
+		:manganese,
+		:chromium,
+		:molybednum,
+		:caffeine,
+		:alcohol,
+		:potassium,
+		:folic_acid,
+		:boron,
+		:cobalt,
+		:fluoride,
+		:acetic_acid,
+		:citric_acid,
+		:lactic_acid,
+		:malic_acid,
+		:choline,
+		:taurine,
+		:glutamine,
+		:creatine,
+		:sugar_alcohols,
+		:chloride,
+		greater_than_or_equal_to: 0
 
 
-  #
-  # Used to override the default to_params so that it includes the name of the food
-  #
-  def to_param
-    [id, name.parameterize].join("-")
-  end
+	#
+	# Used to override the default to_params so that it includes the name of the food
+	#
+	def to_param
+		[id, name.parameterize].join("-")
+	end
 
 
-  #
-  # Finds a Food based on the name (e.f. '112-fried-chicken-breast'
-  #   input: the string
-  #
-  # Ruby will take the string and turn it into a number. 
-  # Internally it will essentially just grab the first numbers and ignore the rest, so simply cast it as an integer
-  def self.find_by_param(input)
-    find(input.to_i)
-  end
+	#
+	# Finds a Food based on the name (e.f. '112-fried-chicken-breast'
+	#   input: the string
+	#
+	# Ruby will take the string and turn it into a number. 
+	# Internally it will essentially just grab the first numbers and ignore the rest, so simply cast it as an integer
+	def self.find_by_param(input)
+		find(input.to_i)
+	end
 
 
-  #
-  # Searches for food items with the name or brand like the passed query. Very crude SQL implementation using LIKE
-  #
-  def self.search_for(query_string)
-    if query_string
-      where('lower(name) ILIKE ? OR lower(brand) ILIKE ?', "%#{query_string.downcase}%", "%#{query_string.downcase}%")
-    else
-      Food.none
-    end
-  end
+	#
+	# Searches for food items with the name or brand like the passed query. Very crude SQL implementation using LIKE
+	#
+	def self.search_for(query_string)
+		if query_string
+			where('lower(name) ILIKE ? OR lower(brand) ILIKE ?', "%#{query_string.downcase}%", "%#{query_string.downcase}%")
+		else
+			Food.none
+		end
+	end
 end
