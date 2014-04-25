dab52d4b84929f1b5a49b07f74d96529834ae5a7
MunkiPhD <munkiphd@gmail.com>
2014-04-24 20:07:48 -0400

    refactor and improvements to graph for each logged workout-exercise

--- app/assets/javascripts/workout_graphs.js
+++ app/assets/javascripts/workout_graphs.js
@@ -1,16 +1,37 @@
 var WorkoutGraphs = window.WorkoutGraphs || {};
 
-WorkoutGraphs.DualYAxisGraph = function(argsObj){
+WorkoutGraphs.CreateDataForRepsAndWeightPerSetGraph = function(workoutExerciseData, containerId){
+	var reps = [];
+	var weight = [];
+	for(var i = 0; i < workoutExerciseData.length; i++){
+		reps.push(workoutExerciseData[i].rep_count);
+		weight.push(workoutExerciseData[i].weight);
+	}
+
+	console.log(workoutExerciseData);
+	console.log("Reps: " + reps);
+	console.log("Weight: " + weight);
+
+	var arguments = {
+		y1Data: reps,
+		y2Data: weight,
+		y1Title: "Reps",
+		y2Title: "Weight",
+		xTitle: "Set Number"
+	};
+	WorkoutGraphs.RepsAndWeightPerSetGraph(arguments, containerId); 
+
+}
+
+WorkoutGraphs.RepsAndWeightPerSetGraph = function(argsObj, containerId){
 	var data1 = argsObj.y1Data;
 	var data2 = argsObj.y2Data;
 	var y1Title = argsObj.y1Title;
 	var y2Title = argsObj.y2Title;
 	var xTitle = argsObj.xTitle;
-	
-	//var data1 = [10,5,5,5,5,4,4,2,2,10];
-	//var data2 = [45,135,135,185,225,315,315,335,385,225];
 
-	var margin = [80,80,80,80];
+
+	var margin = [60,65,60,65];
 	var width = 500 - margin[1] - margin[3];
 	var height = 400 - margin[0] - margin[2];
 
@@ -29,57 +50,95 @@ WorkoutGraphs.DualYAxisGraph = function(argsObj){
 	var line1 = d3.svg.line()
 	// assign the X function to plot our line as we wish
 	.x(function (d, i) {
-	    // return the X coordinate where we want to plot this datapoint
-	    return x(i+1);
+		// return the X coordinate where we want to plot this datapoint
+		return x(i+1);
 	})
-	    .y(function (d) {
-	    // return the Y coordinate where we want to plot this datapoint
-	    return y1(d);
+	.y(function (d) {
+		// return the Y coordinate where we want to plot this datapoint
+		return y1(d);
 	})
 	// create a line function that can convert data[] into x and y points
 	var line2 = d3.svg.line()
 	// assign the X function to plot our line as we wish
 	.x(function (d, i) {
-	    // return the X coordinate where we want to plot this datapoint
-	    return x(i+1);
+		// return the X coordinate where we want to plot this datapoint
+		return x(i+1);
 	})
-	    .y(function (d) {
-	    // verbose logging to show what's actually being done
-	    return y2(d);
+	.y(function (d) {
+		// verbose logging to show what's actually being done
+		return y2(d);
 	})
 
 
 	// Add an SVG element with the desired dimensions and margin.
-	var graph = d3.select("#graph").append("svg:svg")
-	    .attr("width", width + margin[1] + margin[3])
-	    .attr("height", height + margin[0] + margin[2])
-	    .append("svg:g")
-	    .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
+	var graph = d3.select(containerId).append("svg:svg")
+	.attr("width", width + margin[1] + margin[3])
+	.attr("height", height + margin[0] + margin[2])
+	.attr("class", "workout-exercise-rep-weight-graph")
+	.append("svg:g")
+	.attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
+
+	//add the axes labels
+	graph.append("text")
+	.attr("class", "axis-label")
+	.attr("text-anchor", "end")
+	.attr("x", width / 2 + 40)
+	.attr("y", height + 36)
+	.text(xTitle);
+	//
+	//add the axes labels
+	graph.append("text")
+	.attr("class", "axis-label")
+	.attr("style", "font-weight: bold")
+	.attr("text-anchor", "end")
+	.attr("x", width / 2 + 40)
+	.attr("y", -36)
+	.text("Reps & Weight per Set");
+
+	graph.append("text")
+	.attr("class", "axis-label axisRight")
+	.attr("text-anchor", "end")
+	.attr("y", -30)
+	.attr("x", -(height / 2) + 15)
+	.attr("transform", "rotate(-90)")
+	.text(y1Title);
+
+	graph.append("text")
+	.attr("class", "axis-label axisLeft")
+	.attr("text-anchor", "end")
+	.attr("y", width + 55)
+	.attr("x", -(height / 2) + 25)
+	.attr("transform", "rotate(-90)")
+	.text(y2Title);
 
 	// create yAxis
-	var xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true);
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.tickSize(-height)
+		.tickFormat(d3.format("d"))
+		.tickSubdivide(true);
 	// Add the x-axis.
 	graph.append("svg:g")
-	    .attr("class", "x axis")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(xAxis);
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis);
 
 
 	// create left yAxis
 	var yAxisLeft = d3.svg.axis().scale(y1).ticks(4).orient("left");
 	// Add the y-axis to the left
 	graph.append("svg:g")
-	    .attr("class", "y axis axisLeft")
-	    .attr("transform", "translate(-15,0)")
-	    .call(yAxisLeft);
+	.attr("class", "y axis axisLeft")
+	.attr("transform", "translate(0,0)")
+	.call(yAxisLeft);
 
 	// create right yAxis
 	var yAxisRight = d3.svg.axis().scale(y2).ticks(6).orient("right");
 	// Add the y-axis to the right
 	graph.append("svg:g")
-	    .attr("class", "y axis axisRight")
-	    .attr("transform", "translate(" + (width + 15) + ",0)")
-	    .call(yAxisRight);
+	.attr("class", "y axis axisRight")
+	.attr("transform", "translate(" + width + ",0)")
+	.call(yAxisRight);
 	// add lines
 	// do this AFTER the axes above so that the line is above the tick-lines
 	graph.append("svg:path").attr("d", line1(data1)).attr("class", "data1");
