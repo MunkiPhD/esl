142fe3aa2090c3a0fc55b9065dfdfb140355b9b6
MunkiPhD <munkiphd@gmail.com>
2014-05-10 11:30:35 -0400

    passed first feature spec

--- app/controllers/body_weights_controller.rb
+++ app/controllers/body_weights_controller.rb
@@ -1,5 +1,25 @@
 class BodyWeightsController < ApplicationController
+	before_filter :authenticate_user!
+
 	def index
 		@body_weight = BodyWeight.new
+		@body_weights = current_user.body_weights
+	end
+
+	def create
+		@body_weight = current_user.body_weights.build(body_weight_params)
+
+		respond_to do |format|
+			if @body_weight.save
+				format.html { redirect_to body_weights_path, notice: "Body Weight entry logged!" }
+			else
+				format.html { render :index }
+			end
+		end
+	end
+
+	private
+	def body_weight_params
+		params.require(:body_weight).permit(:weight)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Esl::Application.routes.draw do
 
 	resources :exercises
 	resources :workouts
-	resources :body_weights, only: [:index]
+	resources :body_weights, only: [:index, :create]
 
 	#devise_for :users
 	devise_for :users, :controllers => {:registrations => "users/registrations", :passwords => "users/passwords", :sessions => "users/sessions"}
