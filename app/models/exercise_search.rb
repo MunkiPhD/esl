9d5ef771783f87baabd6b427ed9f1170aaeb7078
MunkiPhD <munkiphd@gmail.com>
2014-05-28 09:59:37 -0400

    refactored and prettied up the view

--- app/models/exercise_search.rb
+++ app/models/exercise_search.rb
@@ -4,7 +4,7 @@ class ExerciseSearch
 
 		exercises = exercises.for_exercise_type(params[:exercise_types]) unless params[:exercise_types].nil?
 		exercises = exercises.for_muscle(params[:muscles]) unless params[:muscles].nil?
-		exercises = exercises.for_equipment(params[:equipments]) unless params[:equipments].nil?
+		exercises = exercises.for_equipment(params[:equipment]) unless params[:equipment].nil?
 		exercises = exercises.for_force_type(params[:force_types]) unless params[:force_types].nil?
 		exercises = exercises.for_experience_level(params[:experience_levels]) unless params[:experience_levels].nil?
 		exercises
