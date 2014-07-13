5f8e2902a71ae5d122d7cb989a5a618c023700a0
MunkiPhD <munkiphd@gmail.com>
2014-07-13 09:32:37 -0400

    added spec

--- spec/features/body_measurements/user_manages_body_measurements_spec.rb
+++ spec/features/body_measurements/user_manages_body_measurements_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper.rb'
 
 feature 'user manages their body measurements' do
+	let(:user) { create(:user) }
+
+	before :each do
+		login_user user
+	end
+
 	scenario 'can log a single body measurement' do
 		visit root_path
 		click_link "Body Measurements"
@@ -31,16 +37,42 @@ feature 'user manages their body measurements' do
 
 
 	scenario 'can edit existing measurement data' do
-		fail		
+		body_measurement = create(:body_measurement, user: user)
+		visit body_measurement_path(body_measurement)
+
+		expected_str = "Chest: #{body_measurement.chest} in."
+		expect(page).to have_content expected_str
+
+		click_link "Edit"
+
+		new_measurement = "#{body_measurement.chest + 1}.0"
+		fill_in "Chest", with: new_measurement
+
+		click_button "Save Measurements"
+
+		expect(page).to have_content  "Body measurements were updated."
+		expect(page).to have_content ("Chest: " + new_measurement + " in.")
 	end
 
 
-	scenario 'can delete an existing measurement point of data' do
-		fail
+	scenario 'can delete an existing body measurements entry' do
+		body_measurement = create(:body_measurement, user: user)
+		visit body_measurement_path(body_measurement)
+
+		expect {
+		click_button "Delete"
+		}.to change(BodyMeasurements, :count).by(-1)
+
+		expect(page).to have_content "Body measurements entry was deleted."
 	end
 
 
 	scenario 'defaults to the current date when creating a new entry point' do
+		fail
+	end
+
+
+	scenario 'the unit for the measurement is the same as the user preferences' do
 		fail
 	end
 end
