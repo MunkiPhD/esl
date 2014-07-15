9b74fb4e0b7049f29405106b59012ee7af549aa2
MunkiPhD <munkiphd@gmail.com>
2014-07-15 18:21:21 -0400

    added more specs

--- spec/features/body_measurements/user_manages_body_measurements_spec.rb
+++ spec/features/body_measurements/user_manages_body_measurements_spec.rb
@@ -60,7 +60,7 @@ feature 'user manages their body measurements' do
 		visit body_measurement_path(body_measurement)
 
 		expect {
-		click_button "Delete"
+			click_button "Delete"
 		}.to change(BodyMeasurements, :count).by(-1)
 
 		expect(page).to have_content "Body measurements entry was deleted."
@@ -68,11 +68,44 @@ feature 'user manages their body measurements' do
 
 
 	scenario 'defaults to the current date when creating a new entry point' do
-		fail
+		visit body_measurements_path
+		Timecop.freeze(Date.today) do
+			click_link "Record Measurements"
+			expect(find("#log_date").value).to eq "#{Date.today.strftime('%d-%B-%Y')}"
+		end
 	end
 
+	scenario 'clicking on a measurement point displays a graph and the last ten measurements with that point' do
+		body_measurement create(:body_measurement, user: user)
+		visit body_measurement_path(body_measurement)
+
+		for i in 0..11 do
+			create(:body_measurement, user: user)
+		end
+
+		click_link "Chest"
+
+		expect(page).to have_content("Chest Size")
+		expect(find(".measurement").count).to eq 10
+	end
+
+	scenario 'user can navigate to the body measurement from each individual mesurement point' do
+		Timecop.freeze(Date.today) do
+			body_measurement = create(:body_measurement, user: user, log_date: Date.today)
+			body_measurement_yesterday = create(:body_measurement, user: user, log_date: Date.yesterday)
+
+			visit body_measurement_path(body_measurement)
+
+			click_link "Chest"
+			expect(page).to have_content "#{Date.today.strftime('%d-%B-%Y')}"
+
+			find("a.measurement")[1].click
+
+			expect(page).to have_content "#{Date.yesterday.strftime('%d-%B-%Y')}"
+		end
+	end
 
 	scenario 'the unit for the measurement is the same as the user preferences' do
-		fail
+		fail		
 	end
 end
