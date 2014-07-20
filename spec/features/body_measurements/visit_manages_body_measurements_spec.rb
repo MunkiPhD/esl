de23c6c80174b99430716b0031dc73df489cf080
MunkiPhD <munkiphd@gmail.com>
2014-07-20 19:08:38 -0400

    visitor spec for accessing a measurement entry that doesnt belong to them

--- spec/features/body_measurements/visit_manages_body_measurements_spec.rb
+++ spec/features/body_measurements/visit_manages_body_measurements_spec.rb
@@ -6,4 +6,11 @@ feature 'Visitor manages body measurements' do
 		expect(page).to have_content "sign in or sign up"
 		expect(page).to have_link 'login'
 	end
+
+	scenario 'attempts to view an entry for another user' do
+		body_measurement = create(:body_measurement)
+		visit body_measurement_path(body_measurement)
+		expect(page).to have_content "sign in or sign up"
+		expect(page).to have_link "login"
+	end
 end
