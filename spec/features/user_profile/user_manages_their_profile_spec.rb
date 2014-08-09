575bea08502cd3fb2cd4dd1202ade8ce8f933d01
MunkiPhD <munkiphd@gmail.com>
2014-08-09 17:41:47 -0400

    added spec

--- spec/features/user_profile/user_manages_their_profile_spec.rb
+++ spec/features/user_profile/user_manages_their_profile_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 feature 'User manages their profile info' do
 	let(:user) { create(:user) }
+
 	before :each do
 		login_user user
 	end
 
 	scenario 'user can visit their profile page' do
 		visit root_path
-		expect(page).to have_link user.username
 		click_link user.username
 
-		expect(page).to have_content "My Profile"
+		expect(page).to have_content "My Info"
 	end
 
-	scenario 'edits their profile information' do
+
+	scenario 'edits their profile information for to a male' do
 		visit athletes_path(user)
 		
 		click_link "Edit"
@@ -31,6 +32,26 @@ feature 'User manages their profile info' do
 
 		expect(page).to have_content "5'10\""
 		expect(page).to have_content "Male"
+		expect(page).to have_content format_date(date)
+	end
+
+	scenario 'edits their profile information for to a female' do
+		visit athletes_path(user)
+		
+		click_link "Edit"
+		date = 22.years.ago
+		select_log_date(date, "date_of_birth")
+		within "#height" do
+			fill_in "Feet", with: "4"
+			fill_in "Inches", with: "8"
+		end
+
+		select "Female", from: "Gender"
+
+		click_button "Save"
+
+		expect(page).to have_content "4'8\""
+		expect(page).to have_content "Female"
 		expect(page).to have_content format_date(date)
 	end
 
