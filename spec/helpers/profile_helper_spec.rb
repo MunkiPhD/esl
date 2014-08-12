06f58427904ad03b385aa29f3b2fa440459a8ea1
MunkiPhD <munkiphd@gmail.com>
2014-08-12 08:48:34 -0400

    added age helper

---
+++ spec/helpers/profile_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe ProfileHelper do
+	describe '#age' do
+		it 'displays the correct age based on a date' do
+			Timecop.freeze(Date.new(2010, 4, 4)) do
+				dob = Date.new(1976, 4, 10)
+				result = helper.age(dob)
+				expect(result).to eq 33
+			end
+		end
+
+		it 'displays the correct age on a date 2' do 
+			Timecop.freeze(Date.new(2010, 4, 14)) do
+				dob = Date.new(1976, 4, 10)
+				result = helper.age(dob)
+				expect(result).to eq 34
+			end
+		end
+	end
+end
