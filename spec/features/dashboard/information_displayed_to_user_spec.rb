0e3aa17c12c2933e821feac1dfc66fcc1a10a33a
MunkiPhD <munkiphd@gmail.com>
2014-08-14 07:58:37 -0400

    added pending specs

---
+++ spec/features/dashboard/information_displayed_to_user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'Information displayed on the dashboard' do
+	scenario 'where the user does not have their personal information entered, does not show the BMI, but rather a message' do
+		pending
+	end
+
+	scenario 'where the user has the information required, it shows the BMI' do
+		pending
+	end
+end
