7b8e02720882350da82408ad231b988df638607c
MunkiPhD <munkiphd@gmail.com>
2014-08-17 20:41:24 -0400

    added feature specs

--- spec/features/dashboard/information_displayed_to_user_spec.rb
+++ spec/features/dashboard/information_displayed_to_user_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
 feature 'Information displayed on the dashboard' do
-	scenario 'where the user does not have their personal information entered, does not show the BMI, but rather a message' do
-		pending
+	let(:user) { create(:user) }
+	before :each do
+		login_user user
 	end
 
-	scenario 'where the user has the information required, it shows the BMI' do
-		pending
+	scenario 'where the user does not have age, height, or weight entered, BMI is not displayed' do
+		user.birth_date = nil
+		user.save!
+
+		visit root_path
+
+		within("#bmi") do
+			expect(page).to have_content "not enough information"
+		end
+	end
+
+	scenario 'where the user has age, height, and weight it shows the BMI' do
+		user.age = 20
+		user.height = 70
+		user.save
+
+		weight = create(:body_weight, user: user, weight: 200)
+		visit root_path
+
+		within("#bmi") do 
+			expect(page).to have_content user.bmi
+			expect(page).to_not have_content "not enough information"
+		end
 	end
 end
