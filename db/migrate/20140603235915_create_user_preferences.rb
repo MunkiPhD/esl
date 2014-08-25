998ec46f03dd584f020b6b30964c541cefc0c367
MunkiPhD <munkiphd@gmail.com>
2014-08-24 21:13:52 -0400

    changed default, but it isnt working

--- db/migrate/20140603235915_create_user_preferences.rb
+++ db/migrate/20140603235915_create_user_preferences.rb
@@ -1,12 +1,12 @@
 class CreateUserPreferences < ActiveRecord::Migration
   def change
-    create_table :user_preferences do |t|
-			t.references :user, null: false
-			t.integer :default_system_id, null: false, default: 0
+	  create_table :user_preferences do |t|
+		  t.references :user, null: false
+		  t.integer :default_system_id, null: false, default: MeasurementSystem::US_SYSTEM
 
-      t.timestamps
-    end
+		  t.timestamps
+	  end
 
-		add_index :user_preferences, :user_id
+	  add_index :user_preferences, :user_id
   end
 end
