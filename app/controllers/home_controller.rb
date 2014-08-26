4c183af48915d297a0e8e2bb194b6323c3fdf3de
MunkiPhD <munkiphd@gmail.com>
2014-08-25 20:04:55 -0400

    modified bmi on dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @memberships = Circle.memberships_for_user(current_user)
+		@weight_entry_count = current_user.body_weights.count
     end 
   end
 end
--- spec/features/dashboard/information_displayed_to_user_spec.rb
+++ spec/features/dashboard/information_displayed_to_user_spec.rb
@@ -17,6 +17,38 @@ feature 'Information displayed on the dashboard' do
 		end
 	end
 
+	scenario 'where a user does not have birth date, it asks the user to record their birth date' do
+		user.birth_date = nil
+		user.save!
+
+		visit root_path
+
+		within("#bmi") do
+			expect(page).to have_link "Set your age"
+		end
+	end
+
+	scenario 'where a user does not have a height, it asks the user to record their height' do
+		user.height = nil
+		user.save!
+
+		visit root_path
+
+		within("#bmi") do
+			expect(page).to have_link "Set your height"
+		end
+	end
+
+	scenario 'when a user does not have a weight, it asks the user to record a weight' do
+		expect(user.body_weights.count).to eq 0
+
+		visit root_path
+
+		within("#bmi") do
+			expect(page).to have_link "Record your weight"
+		end
+	end
+
 	scenario 'where the user has age, height, and weight it shows the BMI' do
 		user.birth_date = 20.years.ago + 1
 		user.height = 70
