752335c02dea5a06d669b4db18300dd1557ac70b
MunkiPhD <munkiphd@gmail.com>
2014-08-27 09:20:34 -0400

    formatting of numbers

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,13 +1,13 @@
 module FoodsHelper
   def display_vitamin(display_name, value)
     unless value == 0
-      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{value.to_s}%</td></tr>")
+      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{number_with_precision(value, :precision => 0, :significant => false, :strip_insignificant_zeroes => true)}%</td></tr>")
     end
   end
 
   def display_custom(display_name, value, unit)
     unless value == 0
-      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{value.to_s}#{unit}</td></tr>")
+      raw("<tr><td>#{display_name}</td><td class=\"align_right\">#{number_with_precision(value, :precision => 0, :significant => false, :strip_insignificant_zeroes => true)}#{unit}</td></tr>")
     end
   end
 end
