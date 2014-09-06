4df63a49f85d92f6d3d2d7526776001385d5274b
MunkiPhD <munkiphd@gmail.com>
2014-09-05 22:14:04 -0400

    modified production initializer to pull the s3 credentials from secrets yaml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,13 @@ Esl::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-	config.paperclip_defaults = {
-    storage: 's3',
-    url: "images/:hash:styles.:extension",
-    hash_secret: Rails.application.secrets.image_secret_hash,
-    path: "images/:hash:styles.:extension",
-    s3_credentials: "#{::Rails.root}/config/aws.yml"
-	}
+  config.paperclip_defaults = {
+	  storage: 's3',
+	  url: "images/:hash:styles.:extension",
+	  hash_secret: Rails.application.secrets.image_secret_hash,
+	  path: "images/:hash:styles.:extension",
+	  s3_credentials: { access_key_id: Rails.application.secrets.s3_access_key_id, 
+		  secret_access_key: Rails.application.secrets.s3_secret_access_key, 
+		  bucket: Rails.application.secrets.s3_bucket }
+  }
 end
