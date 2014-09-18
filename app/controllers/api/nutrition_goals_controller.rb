48bd96d64ea20a924564de9bc228ab782ffae3d2
MunkiPhD <munkiphd@gmail.com>
2014-09-17 23:53:44 -0400

    funky test that passes

--- app/controllers/api/nutrition_goals_controller.rb
+++ app/controllers/api/nutrition_goals_controller.rb
@@ -5,4 +5,14 @@ class API::NutritionGoalsController < ApplicationController
 		selected_date = Date.today
 		@presenter = Nutrition::DashboardPresenter.new(current_user, selected_date)
 	end
+=begin
+  def selected_date
+    d = Date.new(date_params[:year].to_i, date_params[:month].to_i, date_params[:day].to_i) rescue nil
+    if d
+      return d
+    else
+      return Date.today
+    end
+  end
+=end
 end
--- spec/controllers/api/nutrition_goals_controller_spec.rb
+++ spec/controllers/api/nutrition_goals_controller_spec.rb
@@ -15,6 +15,7 @@ describe API::NutritionGoalsController, type: :controller do
 			expect(response).to have_http_status(:success)
 		end
 
+
 		it 'returns the info for the daily totals on todays date' do
 			Timecop.freeze(Date.today) do
 				log_food = create(:log_food, user: user, servings: 1)
@@ -25,6 +26,20 @@ describe API::NutritionGoalsController, type: :controller do
 				expect(parsed_json["daily_totals"]["protein"]).to eq log_food.protein
 				expect(parsed_json["daily_totals"]["total_fat"]).to eq log_food.total_fat
 				expect(parsed_json["daily_totals"]["carbs"]).to eq log_food.carbs
+			end
+		end
+
+		it 'returns the daily totals for the specified date' do
+			Timecop.freeze(Date.today) do
+				log_food = create(:log_food, user: user, servings: 2, log_date: 2.days.ago)
+				p log_food.log_date
+				get :index, { format: "json", selected_date: { year: 2.days.ago.year, month: 2.days.ago.month, day: 2.days.ago.day }}
+
+				parsed_json = JSON.parse(response.body)
+				expect(parsed_json["daily_totals"]["calories"]).to eq log_food.calories * 2
+				expect(parsed_json["daily_totals"]["protein"]).to eq log_food.protein * 2
+				expect(parsed_json["daily_totals"]["total_fat"]).to eq log_food.total_fat * 2
+				expect(parsed_json["daily_totals"]["carbs"]).to eq log_food.carbs * 2
 			end
 		end
 
