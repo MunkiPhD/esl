48df95926e3d8f7193a959107766aaa897e43074
MunkiPhD <munkiphd@gmail.com>
2014-10-04 19:15:26 -0400

    added test

--- spec/controllers/api/foods_controller_spec.rb
+++ spec/controllers/api/foods_controller_spec.rb
@@ -13,5 +13,18 @@ describe API::FoodsController, type: :controller do
 			get :search, format: "json"
 			expect(response).to have_http_status(:success)
 		end
+
+		it 'returns search results' do
+			food_one = create(:food, name: "test")
+			food_two = create(:food, name: "bread")
+
+			get :search, { format: "json", search: "est" }
+			parsed_json = JSON.parse(response.body)
+			expect(response.status).to eq 201
+
+			json_food = parsed_json[0]
+			expect(json_food["name"]).to eq food_one.name
+			expect(json_food["id"]).to eq food_one.id
+		end
 	end
 end
