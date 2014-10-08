0781dd0dd6904d3672548e9b8cb1a0233544ca1b
MunkiPhD <munkiphd@gmail.com>
2014-10-08 17:00:02 -0400

    added food search to dashboard

--- spec/features/dashboard/dasboard_spec.rb
+++ spec/features/dashboard/dasboard_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'dasboard features' do
+feature 'dasboard' do
 	let(:user) { create(:user) }
 	before :each do
 		login_user user
@@ -10,5 +10,21 @@ feature 'dasboard features' do
 		visit nutrition_path
 		click_link "dashboard"
 		expect(page).to have_content "Dashboard"
+	end
+
+	scenario 'has the ability to search for food items' do
+		visit root_path
+		food = create(:food, name: "beef")
+
+		within("#nutrition_search_box") do
+			fill_in 'search', with: "beef"
+			click_button "Search"
+		end
+
+		expect(page).to have_link "beef"
+	end
+
+	scenario 'displays the link to log a workout' do
+		fail	
 	end
 end
