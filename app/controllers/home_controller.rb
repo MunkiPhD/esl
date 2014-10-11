daa57863600fe8b2bd957b4cf19c862c4866375d
MunkiPhD <munkiphd@gmail.com>
2014-10-10 21:22:31 -0400

    added food dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     if current_user
       @memberships = Circle.memberships_for_user(current_user)
 		@weight_entry_count = current_user.body_weights.count
+		@presenter = Nutrition::DashboardPresenter.new(current_user, Date.today)
     end 
   end
 end
