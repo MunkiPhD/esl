a4c30f7d675ef0ccf2b6a2c4abe9f4a13c2ec4ce
MunkiPhD <munkiphd@gmail.com>
2014-10-21 19:00:09 -0400

    spec file

---
+++ spec/features/analysis/analysis_between_workouts_nutrition_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature "Analysis" do
+	scenario 'can be accessed via main navigation' do
+		fail
+	end
+
+	scenario 'allows the user to compare calories and workouts on daily basis over time' do
+		fail
+	end
+
+	scenario 'can be done over periods of time' do
+		fail
+	end
+
+	scenario 'has average line for each line' do
+		fail
+	end
+end
